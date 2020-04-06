51e1ad38695122d2c1159e25ae3a4aef67d5aa56
Troy Sampson <troy.j.sampson@gmail.com>
2020-04-06 03:53:04 -0600

    upload changelog

--- scripts/changelog-upload.sh
+++ scripts/changelog-upload.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-
-url=$(curl -v --request POST --header "PRIVATE-TOKEN: ${BUILD_ADMIN_KEY}" --data-urlencode file@CHANGELOG.md ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/uploads | jq .url | sed -e 's/^"//' -e 's/"$//')
-
-echo $url
+curl -v --request POST --header "PRIVATE-TOKEN: ${BUILD_ADMIN_KEY}" --data-urlencode file@CHANGELOG.md ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/uploads | jq .url | sed -e 's/^"//' -e 's/"$//'
