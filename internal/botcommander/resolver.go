6a71e7310c51756dea9724bce09e2926d7e58866
Troy Sampson <troy.j.sampson@gmail.com>
2020-05-25 20:56:37 -0600

    sonar

--- internal/botcommander/resolver.go
+++ internal/botcommander/resolver.go
@@ -21,23 +21,24 @@ func NewResolver() Resolver {
 	return &EvebotResolver{}
 }
 
-// Resolve resolves the input command and returns a valid EvebotCommand or an error
+// Resolve resolves the command input from the Chat User and returns an EvebotCommand
+// this is where all of the "magic" happens that basically translates a user command to an EveBot command
 func (ebr *EvebotResolver) Resolve(input string) botcommands.EvebotCommand {
-	// parse the input string
+	// parse the input string and break out into fields (array)
 	msgFields := strings.Fields(input)
-	// equivalent to just `@evebot`
 	if len(msgFields) == 1 {
+		// equivalent to just `@evebot`
 		// botIDField := msgFields[0]
 		return botcommands.NewRootCmd()
 	}
 
 	cmdFields := msgFields[1:]
 
-	// make sure after you create new command, you add the New func
-	// to the map so that is get's picked up here!
+	// make sure after you create a new command,
+	// you add the New func to the map so that it is picked up here
 	newCmdFuncInterface := botcommands.CommandInitializerMap[cmdFields[0]]
 
-	// Just make sure your New Command func follows the standard New Command signature
+	// Make sure the New Command func follows the standard New Command signature
 	// =======> func NewCmd(input []string) EvebotCommand { }
 	if newCmdFuncInterface != nil {
 		if newCmdFuncVal, ok := newCmdFuncInterface.(func([]string) botcommands.EvebotCommand); ok {
