0374f738b2febfda4df7c8c2710c20e81ca3809a
Troy Sampson <troy.j.sampson@gmail.com>
2020-06-04 09:59:02 -0600

    available label with service vs artifact name

--- internal/eveapi/messaging.go
+++ internal/eveapi/messaging.go
@@ -5,8 +5,6 @@ import (
 	"strings"
 
 	"gitlab.unanet.io/devops/eve/pkg/eve"
-	"gitlab.unanet.io/devops/eve/pkg/log"
-	"go.uber.org/zap"
 )
 
 func headerMsg(val string) string {
@@ -14,8 +12,11 @@ func headerMsg(val string) string {
 }
 
 func availableLabel(svc *eve.DeployService) string {
-	log.Logger.Debug("available label", zap.Any("deploy_service", *svc))
-	return fmt.Sprintf("\n%s (%s):%s", svc.ArtifactName, svc.ServiceName, svc.AvailableVersion)
+	if svc.ArtifactName == svc.ServiceName {
+		return fmt.Sprintf("\n%s:%s", svc.ServiceName, svc.AvailableVersion)
+	} else {
+		return fmt.Sprintf("\n%s (%s):%s", svc.ServiceName, svc.ArtifactName, svc.AvailableVersion)
+	}
 }
 
 func artifactResultBlock(svcs eve.DeployServices) string {
