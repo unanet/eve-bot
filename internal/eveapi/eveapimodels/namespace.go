466fb42dd055e92de4f8fab3ea2ea6ef73f49191
Troy Sampson <troy.j.sampson@gmail.com>
2020-06-16 22:00:27 -0600

    add env name to test and debug

--- internal/eveapi/eveapimodels/namespace.go
+++ internal/eveapi/eveapimodels/namespace.go
@@ -3,21 +3,24 @@ package eveapimodels
 import (
 	"database/sql"
 
+	"gitlab.unanet.io/devops/eve/pkg/log"
+	"go.uber.org/zap"
+
 	"gitlab.unanet.io/devops/eve/pkg/json"
 )
 
 type Namespace struct {
-	ID                 int          `db:"id"`
-	Name               string       `db:"name"`
-	Alias              string       `db:"alias"`
-	EnvironmentID      int          `db:"environment_id"`
-	EnvironmentName    string       `db:"environment_name"`
-	RequestedVersion   string       `db:"requested_version"`
-	ExplicitDeployOnly bool         `db:"explicit_deploy_only"`
-	ClusterID          int          `db:"cluster_id"`
-	Metadata           json.Text    `db:"metadata"`
-	CreatedAt          sql.NullTime `db:"created_at"`
-	UpdatedAt          sql.NullTime `db:"updated_at"`
+	ID                 int          `json:"id"`
+	Name               string       `json:"name"`
+	Alias              string       `json:"alias"`
+	EnvironmentID      int          `json:"environment_id"`
+	EnvironmentName    string       `json:"environment_name"`
+	RequestedVersion   string       `json:"requested_version"`
+	ExplicitDeployOnly bool         `json:"explicit_deploy_only"`
+	ClusterID          int          `json:"cluster_id"`
+	Metadata           json.Text    `json:"metadata"`
+	CreatedAt          sql.NullTime `json:"created_at"`
+	UpdatedAt          sql.NullTime `json:"updated_at"`
 }
 
 type Namespaces []Namespace
@@ -30,7 +33,12 @@ func (e Namespaces) ToChatMessage() string {
 	msg := ""
 
 	for _, v := range e {
+
+		log.Logger.Info("WTF:", zap.Any("namespace", e))
+
 		msg += v.Alias + " (" + v.RequestedVersion + ")" + "\n\n"
+		msg += " (" + v.EnvironmentName + ")" + "\n\n"
+		msg += " (" + v.Name + ")" + "\n\n"
 	}
 
 	return msg
