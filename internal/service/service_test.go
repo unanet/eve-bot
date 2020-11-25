060d3f68e2d49dfa95bf29061d717051966ddf23
Troy Sampson <troy.j.sampson@gmail.com>
2020-11-24 17:50:57 -0700

    test service

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -89,119 +89,104 @@ func TestProvider_HandleSlackInteraction(t *testing.T) {
 }
 
 type serviceMocks struct {
-	mockChat              chatservice.MockProvider
-	mockResolver          resolver.MockResolver
-	mockExecutor          executor.MockExecutor
-	mockAPI               eveapi.MockClient
+	mockChat              *chatservice.MockProvider
+	mockCmd               *commands.MockEvebotCommand
+	mockResolver          *resolver.MockResolver
+	mockExecutor          *executor.MockExecutor
+	mockAPI               *eveapi.MockClient
 	mockCfg               *config.Config
 	mockAllowedChannelMap map[string]interface{}
 }
 
-func TestProvider_HandleSlackAppMentionEvent(t *testing.T) {
-	type fields struct {
-		ChatService       chatservice.Provider
-		CommandResolver   resolver.Resolver
-		CommandExecutor   executor.Executor
-		EveAPI            eveapi.Client
-		Cfg               *config.Config
-		allowedChannelMap map[string]interface{}
+func newServiceMocks(ctrl *gomock.Controller) *serviceMocks {
+	return &serviceMocks{
+		mockChat:              chatservice.NewMockProvider(ctrl),
+		mockCmd:               commands.NewMockEvebotCommand(ctrl),
+		mockResolver:          resolver.NewMockResolver(ctrl),
+		mockExecutor:          executor.NewMockExecutor(ctrl),
+		mockAPI:               eveapi.NewMockClient(ctrl),
+		mockCfg:               &config.Config{},
+		mockAllowedChannelMap: make(map[string]interface{}),
 	}
+}
+
+var mockSlackEvent = &slackevents.AppMentionEvent{
+	Type:            "test",
+	User:            "someuser",
+	Text:            "show environment",
+	TimeStamp:       "2423423423",
+	ThreadTimeStamp: "2342342343",
+	Channel:         "test",
+	EventTimeStamp:  "2342342343",
+	UserTeam:        "unknown",
+	SourceTeam:      "unknown",
+	BotID:           "test",
+}
+
+var mockChatInfo = commands.ChatInfo{
+	User:        mockSlackEvent.User,
+	Channel:     mockSlackEvent.Channel,
+	CommandName: strings.Split(mockSlackEvent.Text, " ")[0],
+}
+
+var mockChatChannel = chatmodels.Channel{
+	ID:   "some id",
+	Name: "some name",
+}
+
+func TestProvider_HandleSlackAppMentionEvent(t *testing.T) {
+
 	type args struct {
 		ctx context.Context
 		ev  *slackevents.AppMentionEvent
-		req *http.Request
 	}
 
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	mockSlackEvent := &slackevents.AppMentionEvent{
-		Type:            "test",
-		User:            "someuser",
-		Text:            "show environment",
-		TimeStamp:       "2423423423",
-		ThreadTimeStamp: "2342342343",
-		Channel:         "test",
-		EventTimeStamp:  "2342342343",
-		UserTeam:        "unknown",
-		SourceTeam:      "unknown",
-		BotID:           "test",
-	}
-
-	mockChatInfo := commands.ChatInfo{
-		User:        mockSlackEvent.User,
-		Channel:     mockSlackEvent.Channel,
-		CommandName: strings.Split(mockSlackEvent.Text, " ")[0],
-	}
-
-	mockChatOpts := commands.CommandOptions{}
-
-	mockChatChannel := chatmodels.Channel{
-		ID:   "test",
-		Name: "WTF",
-	}
-
-	mockAllowedChannels := make(map[string]interface{})
-
-	mockChatSvc := chatservice.NewMockProvider(ctrl)
-	mockChatSvc.EXPECT().GetChannelInfo(context.Background(), mockSlackEvent.Channel).Return(mockChatChannel, nil)
-
-	mockEveCmd := commands.NewMockEvebotCommand(ctrl)
-	mockEveCmd.EXPECT().Info().Return(mockChatInfo)
-	mockEveCmd.EXPECT().Options().Return(mockChatOpts)
-	mockEveCmd.EXPECT().AckMsg().Return("Ohhh yeah", false)
-	mockEveCmd.EXPECT().IsAuthorized(mockAllowedChannels, mockChatSvc.GetChannelInfo).Return(true)
-
-	mockResolver := resolver.NewMockResolver(ctrl)
-	mockResolver.
-		EXPECT().
-		Resolve(mockSlackEvent.Text, mockSlackEvent.Channel, mockSlackEvent.User).
-		Return(mockEveCmd)
-
-	mockExecutor := executor.NewMockExecutor(ctrl)
-	mockAPI := eveapi.NewMockClient(ctrl)
-
 	tests := []struct {
 		name       string
-		fields     fields
 		args       args
-		wantErr    bool
 		setupMocks func(*serviceMocks)
+		ctrl       *gomock.Controller
 	}{
 		{
-			name: "sad path - invalid post body",
+			name: "happy path",
+			ctrl: ctrl,
 			setupMocks: func(t *serviceMocks) {
-
-			},
-			fields: fields{
-				ChatService:       mockChatSvc,
-				CommandResolver:   mockResolver,
-				CommandExecutor:   mockExecutor,
-				EveAPI:            mockAPI,
-				Cfg:               &config.Config{},
-				allowedChannelMap: map[string]interface{}{},
+				t.mockChat.EXPECT().PostMessageThread(context.Background(), "Ohhh yeah", mockSlackEvent.Channel, mockSlackEvent.ThreadTimeStamp).Return("2342342342")
+				t.mockCmd.EXPECT().Info().Return(mockChatInfo)
+				t.mockCmd.EXPECT().AckMsg().Return("Ohhh yeah", false)
+				t.mockResolver.EXPECT().Resolve(mockSlackEvent.Text, mockSlackEvent.Channel, mockSlackEvent.User).Return(t.mockCmd)
 			},
 			args: args{
 				ctx: context.Background(),
 				ev:  mockSlackEvent,
-				req: httptest.NewRequest("POST", "/somewhere", nil),
 			},
-			wantErr: true,
+		},
+		{
+			name: "slack auth disabled",
+			ctrl: ctrl,
+			setupMocks: func(t *serviceMocks) {
+				t.mockChat.EXPECT().PostMessageThread(context.Background(), "Ohhh yeah", mockSlackEvent.Channel, mockSlackEvent.ThreadTimeStamp).Return("2342342342")
+				t.mockCmd.EXPECT().Info().Return(mockChatInfo)
+				t.mockCmd.EXPECT().AckMsg().Return("Ohhh yeah", false)
+				t.mockResolver.EXPECT().Resolve(mockSlackEvent.Text, mockSlackEvent.Channel, mockSlackEvent.User).Return(t.mockCmd)
+			},
+			args: args{
+				ctx: context.Background(),
+				ev:  mockSlackEvent,
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			p := &Provider{
-				ChatService:       tt.fields.ChatService,
-				CommandResolver:   tt.fields.CommandResolver,
-				CommandExecutor:   tt.fields.CommandExecutor,
-				EveAPI:            tt.fields.EveAPI,
-				Cfg:               tt.fields.Cfg,
-				allowedChannelMap: tt.fields.allowedChannelMap,
+			m := newServiceMocks(tt.ctrl)
+			//tt.setupMocks(m)
+			if tt.setupMocks != nil {
+				tt.setupMocks(m)
 			}
-			if err := p.HandleSlackAppMentionEvent(tt.args.ctx, tt.args.ev); (err != nil) != tt.wantErr {
-				t.Errorf("Provider.HandleSlackInteraction() error = %v, wantErr %v", err, tt.wantErr)
-			}
+			New(m.mockCfg, m.mockResolver, m.mockAPI, m.mockChat, m.mockExecutor).HandleSlackAppMentionEvent(tt.args.ctx, tt.args.ev)
 		})
 	}
 }
