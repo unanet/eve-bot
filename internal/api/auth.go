e90d4e30e67a02d1d797f8d7dd311fb67badc3d8
Troy Sampson <tsampson@sixgill.com>
2021-08-29 17:28:21 -0600

    put user auth

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -70,9 +70,18 @@ func (c AuthController) auth(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+// UserEntry struct to hold info about new user item
+type UserEntry struct {
+	UserID string
+	Email  string
+	Name   string
+	Roles  []string
+	Groups []string
+}
+
 func (c AuthController) callback(w http.ResponseWriter, r *http.Request) {
 	incomingState := r.URL.Query().Get("state")
-	log.Logger.Info("incoming state", zap.Any("state", incomingState))
+	log.Logger.Info("incoming oidc callback state", zap.Any("state", incomingState))
 
 	ctx := r.Context()
 
@@ -90,113 +99,102 @@ func (c AuthController) callback(w http.ResponseWriter, r *http.Request) {
 
 	idToken, err := c.svc.MgrSvc.OpenIDService().Verify(ctx, rawIDToken)
 	if err != nil {
-		render.Respond(w, r, err)
+		render.Respond(w, r, errors.Wrap(err))
 		return
 	}
 
 	var idTokenClaims = new(json.RawMessage)
 	if err := idToken.Claims(&idTokenClaims); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		render.Respond(w, r, errors.Wrap(err))
 		return
 	}
 
 	var claims = make(map[string]interface{})
 	b, err := idTokenClaims.MarshalJSON()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		render.Respond(w, r, errors.Wrap(err))
 		return
 	}
 	err = json.Unmarshal(b, &claims)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		render.Respond(w, r, errors.Wrap(err))
 		return
 	}
-	log.Logger.Info("incoming claims data", zap.Any("claims", claims))
 
-	// Create struct to hold info about new item
-	type UserEntry struct {
-		UserID string
-		Email  string
-		Name   string
-		Roles  []string
-		Groups []string
-	}
+	log.Logger.Debug("incoming claims data", zap.Any("claims", claims))
 
 	ue := &UserEntry{
 		UserID: incomingState,
 		Email:  claims["email"].(string),
 		Name:   claims["preferred_username"].(string),
+		Roles:  extractClaimSlice(claims["roles"]),
+		Groups: extractClaimSlice(claims["group"]),
 	}
 
-	switch v := claims["roles"].(type) {
-	case []string:
-		log.Logger.Info("incoming claims roles slice of string", zap.Any("v", v))
-	case []interface{}:
-		log.Logger.Info("incoming claims roles slice of interfaces", zap.Any("v", v))
-	case interface{}:
-		log.Logger.Info("incoming claims roles interface", zap.Any("v", v))
-	default:
-		log.Logger.Info("incoming claims roles unknown", zap.Any("v", v))
+	log.Logger.Debug("user entry data", zap.Any("user_entry", ue))
+	av, err := dynamodbattribute.MarshalMap(ue)
+	if err != nil {
+		render.Respond(w, r, errors.Wrap(err))
+		return
 	}
 
-	switch g := claims["groups"].(type) {
-	case []string:
-		log.Logger.Info("incoming claims group slice of string", zap.Any("v", g))
-	case []interface{}:
-		log.Logger.Info("incoming claims groups slice of interfaces", zap.Any("g", g))
-	case interface{}:
-		log.Logger.Info("incoming claims groups interface", zap.Any("g", g))
-	default:
-		log.Logger.Info("incoming claims groups unknown", zap.Any("g", g))
-	}
-
-	log.Logger.Info("user data", zap.Any("user_entry", ue))
-
-	result, err := c.svc.UserDB.GetItem(&dynamodb.GetItemInput{
+	userEntry, err := c.svc.UserDB.PutItem(&dynamodb.PutItemInput{
+		Item:      av,
 		TableName: aws.String("eve-bot-users"),
-		Key: map[string]*dynamodb.AttributeValue{
-			"UserID": {
-				S: aws.String(ue.UserID),
-			},
-			"Email": {
-				S: aws.String(ue.Email),
-			},
-		},
 	})
 	if err != nil {
-		log.Logger.Error("failed to get user db entry", zap.Any("user_entry", ue), zap.Error(err))
+		render.Respond(w, r, errors.Wrap(err))
+		return
 	}
+	log.Logger.Info("saved user entry", zap.Any("user_entry", userEntry))
 
-	log.Logger.Info("read bot user result", zap.Any("res", result))
+	//result, err := c.svc.UserDB.GetItem(&dynamodb.GetItemInput{
+	//	TableName: aws.String("eve-bot-users"),
+	//	Key: map[string]*dynamodb.AttributeValue{
+	//		"UserID": {
+	//			S: aws.String(ue.UserID),
+	//		},
+	//		"Email": {
+	//			S: aws.String(ue.Email),
+	//		},
+	//	},
+	//})
+	//if err != nil {
+	//	render.Respond(w, r, err)
+	//	return
+	//}
+	//
+	//log.Logger.Debug("read bot user result", zap.Any("res", result))
+	//
+	//// User does not exist (lets create an entry)
+	//if result == nil || result.Item == nil {
+	//	log.Logger.Info("bot user does not exist")
+	//	av, err := dynamodbattribute.MarshalMap(ue)
+	//	if err != nil {
+	//		render.Respond(w, r, errors.Wrap(err))
+	//		return
+	//	}
+	//	userEntry, err := c.svc.UserDB.PutItem(&dynamodb.PutItemInput{
+	//		Item:      av,
+	//		TableName: aws.String("eve-bot-users"),
+	//	})
+	//	if err != nil {
+	//		render.Respond(w, r, errors.Wrap(err))
+	//		return
+	//	}
+	//	log.Logger.Info("saved bot user", zap.Any("user_entry", userEntry))
+	//} else {
+	//	log.Logger.Info("bot user exists", zap.Any("res", result))
+	//	entry := UserEntry{}
+	//	err = dynamodbattribute.UnmarshalMap(result.Item, &entry)
+	//	if err != nil {
+	//		render.Respond(w, r, errors.Wrap(err))
+	//		return
+	//	}
+	//	log.Logger.Info("read bot user", zap.Any("user_entry", entry))
+	//}
 
-	// User does not exist (lets create an entry)
-	if result == nil || result.Item == nil {
-		log.Logger.Info("bot user does not exist")
-		av, err := dynamodbattribute.MarshalMap(ue)
-		if err != nil {
-			render.Respond(w, r, errors.Wrap(err))
-			return
-		}
-		userEntry, err := c.svc.UserDB.PutItem(&dynamodb.PutItemInput{
-			Item:      av,
-			TableName: aws.String("eve-bot-users"),
-		})
-		if err != nil {
-			render.Respond(w, r, errors.Wrap(err))
-			return
-		}
-		log.Logger.Info("saved bot user", zap.Any("user_entry", userEntry))
-	} else {
-		log.Logger.Info("bot user exists", zap.Any("res", result))
-		entry := UserEntry{}
-		err = dynamodbattribute.UnmarshalMap(result.Item, &entry)
-		if err != nil {
-			render.Respond(w, r, errors.Wrap(err))
-			return
-		}
-		log.Logger.Info("read bot user", zap.Any("user_entry", entry))
-	}
-
+	// Leaving this here for demo/test purposes
 	//render.JSON(w, r, TokenResponse{
 	//	AccessToken:  oauth2Token.AccessToken,
 	//	RefreshToken: oauth2Token.RefreshToken,
@@ -204,11 +202,18 @@ func (c AuthController) callback(w http.ResponseWriter, r *http.Request) {
 	//	Expiry:       oauth2Token.Expiry,
 	//	Claims:       idTokenClaims,
 	//})
-
 	// Just redirecting to a different page to prevent id refresh (which throws an error)
 	http.Redirect(w, r, "/signed-in", http.StatusFound)
 	return
 
+}
+
+func extractClaimSlice(input ...interface{}) []string {
+	var paramSlice []string
+	for _, param := range input {
+		paramSlice = append(paramSlice, param.(string))
+	}
+	return paramSlice
 }
 
 type TokenResponse struct {
